90c44f74cc01566211fb649853c7c7146f7f4107
AliOmidMazariJan <99080700+AliOmidMazariJan@users.noreply.github.com>
2022-02-05 18:33:32 +0430

    OmidJan. py
    git clone https://github.com/AliOmidMazariJan/AFG_AliOmid_Mazari
    ls
    cd AliOmid_Mazari
    ls
    Python2 OmidJan.py

---
+++ OmidJan.py
@@ -0,0 +1,60 @@
+# SH-WORLD
+<p align="center">
+<a href="https://github.com/AliOmidMazariJan/AFG_AliOmid_Mazari/new/main"><img title="M A Z A R Ehttps://github.com/AliOmidMazari" src="https://github-readme-stats.vercel.app/api?username=SHANTO&show_icons=true&include_all_commits=true&theme=chartreuse-dark&cache_seconds=3200"></a>
+</p>
+
+
+<p align="center">
+<a href="#"><img title="succeed" src="https://img.shields.io/badge/deobfuscating-succeed-green?colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<br/><br/>
+
+<h1 align="center"> Ma-zariV1</h1>
+<p align="center">      ACCOUNT CLONER FOR TERMUX USERS</p>
+
+## ***About SH-WORLD  V1***:
+
+SH-WORLD is a python based cloning script. You can clone unlimited Bangladeshi facebook accounts by using this tool. This tool works on any Android devices without root.
+
+## Install on Termux :
+```
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install git
+pkg install pip
+pkg install pip2
+pip2 install requests
+pip2 install mechanize
+https://github.com/AliOmidMazariJan/AFG_AliOmid_Mazari/new/main
+## Install on Terminal :
+```
+apt update && apt upgrade
+apt install python
+apt install python2
+apt install git
+apt install pip
+apt install pip2
+pip2 install requests
+pip2 install mechanize
+https://github.com/AliOmidMazariJan/AFG_AliOmid_Mazari/new/main
+
+## Tools Run :
+```
+ls && cd SH-WORLD && ls
+python2 sh3k.py 
+```
+
+*   USERNAME:  AliOmid 
+*   Passwords : Mazari
+
+
+## ***Follow Me***
+
+* Telegram : [Join Now](https://t.me/RaYeesHcker)
+* Facebook  : [Follow Me](https://www.facebook.com/RayeesOmidjan420)
+* GitHub : [Follow Me](https://github.com/AliOmidMazariJan/AFG_AliOmid_Mazari/new/main)
+
+### Warning
+
+***Don't try to edit or modify this tool. This tool is only for educational purpose. If you use this tool for other purposes except education we will not be responsible in such cases.***
